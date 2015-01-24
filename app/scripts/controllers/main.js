e3d13274a9d9654f7eecab041f633fd4baab96c8
BetinMcFly <c.alberto.solano@gmail.com>
2015-01-23 18:57:45 -0600

    Estos son los archivs base de la lista de tareas en Angularjs

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tareasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tareasApp
+ */
+angular.module('tareasApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.tareas = [ 'Item'];
+  });
